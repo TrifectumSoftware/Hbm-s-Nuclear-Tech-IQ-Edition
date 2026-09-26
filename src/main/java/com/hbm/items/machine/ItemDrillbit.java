ed517c2c3a7d923a61ff9c90cdaf4fffcd6da272
NotMega <iamnotmega@proton.me>
2026-09-26 07:19:58 +0300

    so can we wander for a spell? and live in parallel?

--- src/main/java/com/hbm/items/machine/ItemDrillbit.java
+++ src/main/java/com/hbm/items/machine/ItemDrillbit.java
@@ -19,25 +19,25 @@ public class ItemDrillbit extends ItemEnumMulti {
 	public ItemDrillbit() {
 		super(EnumDrillType.class, true, true);
 	}
-	
+
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void registerIcons(IIconRegister reg) {
 		Enum[] enums = theEnum.getEnumConstants();
 		this.icons = new IIcon[enums.length];
-		
+
 		for(int i = 0; i < icons.length; i++) {
 			Enum num = enums[i];
 			this.icons[i] = reg.registerIcon(this.getIconString() + "_" + num.name().toLowerCase(Locale.US));
 		}
 	}
-	
+
 	@Override
 	public String getUnlocalizedName(ItemStack stack) {
 		Enum num = EnumUtil.grabEnumSafely(theEnum, stack.getItemDamage());
 		return super.getUnlocalizedName() + "_" + num.name().toLowerCase(Locale.US);
 	}
-	
+
 	@Override
 	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean ext) {
 		EnumDrillType type = EnumUtil.grabEnumSafely(theEnum, stack.getItemDamage());
@@ -48,25 +48,30 @@ public class ItemDrillbit extends ItemEnumMulti {
 		if(type.vein) list.add(EnumChatFormatting.GREEN + "Vein miner");
 		if(type.silk) list.add(EnumChatFormatting.GREEN + "Silk touch");
 	}
-	
+
 	public static enum EnumDrillType {
 		STEEL			(1.0D, 1, 0, false, false),
 		STEEL_DIAMOND	(1.0D, 1, 2, false, true),
+		STEEL_VANADIUM  (1.5D, 1, 0, false, false),
 		HSS				(1.2D, 2, 0, true, false),
 		HSS_DIAMOND		(1.2D, 2, 3, true, true),
+		HSS_VANADIUM    (1.7D, 2, 0, true, false),
 		DESH			(1.5D, 3, 1, true, true),
 		DESH_DIAMOND	(1.5D, 3, 4, true, true),
+		DESH_VANADIUM   (2.0D, 3, 0, true, true),
 		TCALLOY			(2.0D, 4, 1, true, true),
 		TCALLOY_DIAMOND	(2.0D, 4, 4, true, true),
+		TCALLOY_VANADIUM(2.5D, 4,  0, true, true),
 		FERRO			(2.5D, 5, 1, true, true),
-		FERRO_DIAMOND	(2.5D, 5, 4, true, true);
-		
+		FERRO_DIAMOND	(2.5D, 5, 4, true, true),
+		FERRO_VANADIUM  (3.0D, 5, 0, true, true);
+
 		public double speed;
 		public int tier;
 		public int fortune;
 		public boolean vein;
 		public boolean silk;
-		
+
 		private EnumDrillType(double speed, int tier, int fortune, boolean vein, boolean silk) {
 			this.speed = speed;
 			this.tier = tier;
